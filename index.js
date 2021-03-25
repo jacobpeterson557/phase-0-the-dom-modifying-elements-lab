69a512453c5d76bfee345c446f21f6f8a9543d33
+ <+>
2021-03-25 11:55:22 -0600

    commit tree fdcbbd6a795bf4d3ea7639ea8efcb2ad29663119

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+document.body.appendChild(newHeader)
+
+newHeader.textContent = 'Jacob is the champion'
